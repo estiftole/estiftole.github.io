aea52704262d51d9aec9923dc582ded701a1b9da
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-09-04 17:40:20 +0300

    set default theme to system

--- src/js/consistent-theme.js
+++ src/js/consistent-theme.js
@@ -2,8 +2,8 @@
   const savedTheme = localStorage.getItem('theme');
   const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-  // if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-  if (savedTheme === 'dark') {
+  if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
+  // if (savedTheme === 'dark') {
     document.documentElement.setAttribute('data-theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
